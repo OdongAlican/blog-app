2e8afd246429fbd1e621f42eb1752d6726a5b6ed
OdongAlican <sandieo.2020@gmail.com>
2020-05-26 13:20:09 +0300

    implements update request

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,4 +26,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+  
+    redirect_to article_path(@article)
+  end
 end
