32d42fd8f02fcc5fad5cb1e12e707a31a1f13a63
OdongAlican <sandieo.2020@gmail.com>
2020-05-26 12:37:16 +0300

    deletes an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,29 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
   end
 end
