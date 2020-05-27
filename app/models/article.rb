966a8826b9f70c39f6d78689314632593642d9da
OdongAlican <sandieo.2020@gmail.com>
2020-05-27 12:13:32 +0300

    finished blog comments feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+    has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'articles#index'
   resources :articles
+  resources :articles do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
